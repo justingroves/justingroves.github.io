9bb1096b4424c9c101ca3b501987afd780e7faa0
Justin Groves <jagroves95@gmail.com>
2025-12-29 16:42:34 -0500

    Added a banner and CSS standardization

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const icons = document.querySelectorAll(".social-icon i");
+  const icons = document.querySelectorAll(".social-icons i");
 
   if (!icons) return; // safety check
 
   icons.forEach((icon) => {
     const randomDelay = () =>
-      // 2.5 - 3.5 minutes (ms)
-      Math.random() * 60000 + 150000;
+      // 1.5 - 2.5 minutes (ms)
+      Math.random() * 30000 + 150000;
 
     const bounce = () => {
       icon.classList.add("icon-bounce");
