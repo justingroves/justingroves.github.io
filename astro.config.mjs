c370fd8ee2d6c33018faacde70f83dbd9228202d
Justin Groves <jagroves95@gmail.com>
2026-06-08 23:17:49 -0400

    Mod: Fix site location config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.justingroves.com",
+  site: "https://justingroves.com",
+  base: "/",
 });
