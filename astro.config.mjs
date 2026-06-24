8fe530375bb4652b16d41482b50f95293916c563
Justin Groves <jagroves95@gmail.com>
2026-06-24 09:36:16 -0400

    Add: Sitemap support for Google SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig, fontProviders } from "astro/config";
-import preact from "@astrojs/preact";
-import mdx from "@astrojs/mdx";
-import tailwindcss from "@tailwindcss/vite";
+import preact from "@astrojs/preact"; // Minimal React Support
+import mdx from "@astrojs/mdx"; // Markdown + JSX Support
+import sitemap from "@astrojs/sitemap"; // SEO Support
+
+import tailwindcss from "@tailwindcss/vite"; // CSS Support
 import icon from "astro-icon";
+
+// LaTeX support
 import remarkMath from "remark-math";
 import rehypeKatex from "rehype-katex";
 // import rehypeMathjax from "rehype-mathjax";
@@ -12,7 +16,7 @@ import { unified } from "@astrojs/markdown-remark";
 // https://astro.build/config
 export default defineConfig({
   site: "https://justingroves.com",
-  integrations: [preact(), mdx(), icon()],
+  integrations: [preact(), mdx(), icon(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
